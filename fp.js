6e72293dfb77617efddf1e265092f3041ed8aa8c
Jesse Shawl <jesse@jshawl.com>
2016-09-26 09:32:27 -0400

    add some fp

---
+++ fp.js
@@ -0,0 +1,35 @@
+// Convert the following code to use `.map`
+
+var peoplesAges = []
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+]
+people.forEach((person) => {
+  peoplesAges.push(person.age)
+})
+
+// Convert the following code to use `.filter`
+
+var peopleYoungerThan35 = []
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+]
+people.forEach((person) => {
+  if(person.age < 35)
+    peopleYoungerThan35.push(person.age)
+})
+
+// Convert the following to use `.forEach`
+
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+]
+var peopleWhoseNamesHaveTheLetterS = people.filter( person => {
+  return person.name.match("s")
+})

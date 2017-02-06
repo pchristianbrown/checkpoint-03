fac0bc8913aa094a849a569ac690254dcabb7eab
Nayana Davis <nayana487@gmail.com>
2017-02-05 23:18:25 -0500

    Update fp.js

--- fp.js
+++ fp.js
@@ -31,4 +31,4 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){ return person.name.match("s")});
+var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});

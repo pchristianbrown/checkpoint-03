025b76b1026490a783a00d48b03002dbbd6fe279
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-05-08 09:56:05 -0400

    3 commit

--- fp.js
+++ fp.js
@@ -1,28 +1,25 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = []
+var peoplesAges = [];
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
 people.forEach(function(person){
-  peoplesAges.push(person.age)
+  peoplesAges.map(person.age)
 });
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
-var people = [
+let peopleYoungerThan35 = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+let age= peopleYoungerThan35.filter(person)=>
+return peopleYoungerThan35.age>35;
+  })
 
 // Convert the following to use `.forEach`
 
@@ -31,4 +28,6 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+var peopleWhoseNamesHaveTheLetterS = [];
+
+ people.forEach(function(person){return person.name.match("s")});
